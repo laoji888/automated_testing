fcc813bda079293a5041724de36babaa2b7f9a0e
laoji <xztlaoji@163.com>
2020-03-27 23:05:22 +0800

    添加启动浏览器方式

--- case/ui_test.py
+++ case/ui_test.py
@@ -1,3 +1,6 @@
+import warnings
+
+from selenium.webdriver import DesiredCapabilities
 from page.home import Home
 from page.recruitment_management import Recruitment_management
 import time, unittest
@@ -8,21 +11,32 @@ from time import sleep
 class CsmsTest(unittest.TestCase):
 
     def setUp(self):
-        #self.driver = webdriver.Firefox()
-        self.driver = webdriver.Remote(
-         	       command_executor='http://106.13.132.197:8888/wd/hub',
-         	       desired_capabilities={'browserName': 'firefox'}
-         	       )
+        # self.driver = webdriver.Firefox()
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        # 调用指定的执行机
+        self.driver = webdriver.Remote(command_executor='192.168.31.152:1112/wd/hub',
+                                       desired_capabilities=DesiredCapabilities.INTERNETEXPLORER)
+
+        # 调用集群浏览器，docker自动分配执行机
+        self.driver = webdriver.Remote(command_executor='http://106.13.132.197:8888/wd/hub',
+                                       desired_capabilities={'browserName': 'firefox',
+                                                             # "version": "",
+                                                             # "platform": "",
+                                                             # "javascriptEnabled": True,
+                                                             # "marionette": True,
+                                                             }
+                                       )
         self.url = "http://120.52.157.131:58080/#/home/cooperation"
-        self.home = Home(self.driver,self.url)
-        self.rm = Recruitment_management(self.driver,self.url)
+        self.home = Home(self.driver, self.url)
+        self.rm = Recruitment_management(self.driver, self.url)
 
     def test_login(self):
         # 乔琳登录
         self.home.login("qiaolin", "ChinaTower1234")
         time.sleep(1)
         v1 = self.home.find_element(*self.home.element(3))
-        self.assertIn("乔琳", v1.text,  msg='错误')
+        self.assertIn("乔琳", v1.text, msg='错误')
 
         # 进入合作伙伴管理下的招募管理
         self.home.enter_workbench()
@@ -33,7 +47,7 @@ class CsmsTest(unittest.TestCase):
         # 申请招募
         self.rm.apply_recruitment("自动化测试0808", "测试")
         v2 = self.rm.find_element(*self.rm.element(13))
-        self.assertIn("成功", v2.text,  msg='错误')
+        self.assertIn("成功", v2.text, msg='错误')
         sleep(3)
 
         # 退出登录后一级审批登录
@@ -60,11 +74,5 @@ class CsmsTest(unittest.TestCase):
         self.assertIn("成功", v4.text, msg='错误')
         sleep(3)
 
-
-
-
     def tearDown(self):
         self.driver.quit()
-
-
-
